33ffcf3e633831c2fd51740eece8ccc10b8a45cb
Feitoza de Azevedo <wrfv@gft.com>
2022-06-08 22:16:58 -0300

    teste de validacao de password

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log("executando arquivo js");
+// o que é Array?
+// Array é uma lista de elementos(numeros, strings, objetos, booleanos)
+//exemplo de array em js
+const array = [1, 2, 3, 4, 5, 6];
+
+// O que são objetos?
+const usuario = {
+  name: "Joao",
+  age: "18",
+  phone: "1999099980",
+};
+
+// o que é array de objetos?
+const usuarios = [
+  {
+    name: "Joao",
+    age: "18",
+    phone: "1999099980",
+  },
+  {
+    name: "Joao2",
+    age: "18",
+    phone: "1999099980",
+  },
+  {
+    name: "Joao3",
+    age: "18",
+    phone: "1999099980",
+  },
+];
+console.log(usuarios)
+    // usuarios.forEach((res, index) => console.log(index, res))
+// como tratar arrays com javascript
+
+// como buscar valores dentro de um objeto
+
+// operadores de comparaçãos
+
+// classes com javascript
+
+// metodos com javascript
+
+// exercicios
